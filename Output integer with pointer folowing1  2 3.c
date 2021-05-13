3765269fab0e731f5885538296c2232582ca1752
周八方 <76588102+Zhoubafang@users.noreply.github.com>
2021-05-13 20:15:46 +0800

    Output integer with pointer folowing1 2 3.c
    例题：定义一个字符指针变量来指向一个字符串，并通过字符指针变量输出改字符串。

--- Output integer with pointer folowing1  2 3.c
+++ Output integer with pointer folowing1  2 3.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-void swap(int* p1, int* p2)
-{
-	int p;
-	p = *p1; *p1 = *p2; *p2 = p;
-}
+
 int main(void)
 {
+	int* p1, * p2, * p3;
+	printf("input three integer n1,b2,n3:")
 
 	int n1, n2, n3;
-	int* p1, * p2, * p3;
-	printf("input three integer n1,n2,n3:");
-	scanf_s("%d %d %d", &n1, &n2, &n3);
-	printf("%d %d %d\n", n1, n2, n3);
-	p1 = &n1;
-	p2 = &n2;
-	p3 = &n3;
-	if (n1 > n2) swap(p1, p2);
-	if (n1 > n3) swap(p1, p3);
-	if (n2 > n3) swap(p2, p3);
-	printf("Now,the order is:%d,%d,%d\n", n1, n2, n3);
+	p = p*
 	return 0;
 }
