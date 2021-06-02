c99c23326c681672e489515850c4d23ab26539ad
周八方 <76588102+Zhoubafang@users.noreply.github.com>
2021-06-02 15:31:09 +0800

    思考：将第一个for循环中的括号去掉三三个for之间的关系，以及与没有去掉之前的区别。

--- 输出金字塔.c
+++ 输出金字塔.c
@@ -6,7 +6,7 @@ int main(void)
 	for(i=1;i<=7;i++)
 	{
 		for(j=1;j<=7-i;j++) printf(" ");
-		for(j=1;j<=2*i-1;j++) printf("*");
+		for(j=1;j<=2*i-1;j++) printf("*");   for循环2,3为并列地位
 		printf("\n");
 	}
 	return 0;
